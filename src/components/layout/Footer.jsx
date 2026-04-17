85b78ad6e4ee1b27e73a741dbabe920c376c4aa9
Pedro Rocha Dantas <pedrord1811@gmail.com>
2026-04-17 03:21:34 -0300

    Footer parte 1

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,25 +1,14 @@
 import React from "react";
+import "./Footer.css";
 
-/**
- * Rodapé simples
- */
 function Footer() {
   return (
-    <footer style={styles.footer}>
-      <p>© 2026 Rifa Fácil</p>
+    <footer className="footer">
+      <p>
+        Rifa Fácil <span>•</span> Desenvolvido por alunos do ICMC-USP
+      </p>
     </footer>
   );
 }
-
-const styles = {
-  footer: {
-    height: "50px",
-    background: "#fff",
-    borderTop: "1px solid #ddd",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-};
 
 export default Footer;
