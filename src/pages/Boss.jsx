90589bc6fc8463c961cec5c847339b4007ee36e5
Gustavo Henrique <ghna.filho@gmail.com>
2026-04-17 02:05:45 -0300

    Arrumei o layout do boss

--- src/pages/Boss.jsx
+++ src/pages/Boss.jsx
@@ -4,6 +4,7 @@ import RaffleCard from "../components/RaffleCard";
 function Boss() {
   const [raffles, setRaffles] = useState([]);
   const [showForm, setShowForm] = useState(false);
+  const [text, settext] = useState(true);
 
   const [newRaffle, setNewRaffle] = useState({
     title: "",
@@ -40,12 +41,10 @@ function Boss() {
     <div style={styles.container}>
       <h1>Gerenciar Rifas</h1>
 
-      {/* BOTÃO */}
       <button style={styles.button} onClick={() => setShowForm(!showForm)}>
         + Criar nova rifa
       </button>
 
-      {/* FORM */}
       {showForm && (
         <div style={styles.form}>
           <h2>Criar nova rifa</h2>
@@ -82,8 +81,27 @@ function Boss() {
           </button>
         </div>
       )}
+      {text && (
+        <div style={styles.summaryCard}>
+          <h2 style={styles.summaryTitle}>Minha Rifa</h2>
 
-      {/* LISTA */}
+          <div style={styles.summaryItem}>
+            <span>📱 Produto:</span>
+            <strong>iPhone</strong>
+          </div>
+
+          <div style={styles.summaryItem}>
+            <span>🎟 Rifas vendidas:</span>
+            <strong>8</strong>
+          </div>
+
+          <div style={styles.summaryItem}>
+            <span>💰 Valor arrecadado:</span>
+            <strong>R$ 120,00</strong>
+          </div>
+        </div>
+      )}
+
       <div style={styles.grid}>
         {raffles.map((raffle) => (
           <div style={styles.cardWrapper} key={raffle.id}>
@@ -100,7 +118,31 @@ const styles = {
     padding: "20px",
     textAlign: "center",
   },
+  summaryCard: {
+  marginTop: "20px",
+  width: "90%",
+  maxWidth: "400px",
+  margin: "20px auto",
+  padding: "20px",
+  background: "#f1f5f9",
+  borderRadius: "12px",
+  boxShadow: "0 4px 10px rgba(0,0,0,0.1)",
+  display: "flex",
+  flexDirection: "column",
+  gap: "10px",
+  textAlign: "left",
+  },
 
+  summaryTitle: {
+    textAlign: "center",
+    marginBottom: "10px",
+  },
+
+  summaryItem: {
+    display: "flex",
+    justifyContent: "space-between",
+    fontSize: "16px",
+  },
   button: {
     padding: "12px 20px",
     marginTop: "10px",
@@ -110,20 +152,23 @@ const styles = {
     borderRadius: "8px",
     cursor: "pointer",
   },
-
+  rifas_adquiridas:{
+    marginTop: "20px",
+    color: "black",
+    border: "solid black 4px"
+  },
   form: {
     marginTop: "20px",
     display: "flex",
     flexDirection: "column",
     gap: "15px",
-    width: "100%",
-    maxWidth: "500px",
-    marginInline: "auto",
-    padding: "30px",
+    width: "90%",
+    maxWidth: "400px", 
+    margin: "0 auto", 
+    padding: "10px",
     background: "#f1f5f9",
     borderRadius: "12px",
-    minHeight: "300px",
-    justifyContent: "center",
+    alignItems: "stretch", 
   },
 
   input: {
@@ -132,6 +177,7 @@ const styles = {
     borderRadius: "8px",
     border: "1px solid #ccc",
     width: "100%",
+    boxSizing: "border-box",
   },
 
   createButton: {
@@ -142,6 +188,8 @@ const styles = {
     borderRadius: "8px",
     cursor: "pointer",
     fontWeight: "bold",
+    width: "100%", 
+    boxSizing: "border-box",
   },
 
   grid: {
